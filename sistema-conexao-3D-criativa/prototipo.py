4c98ef92846688ddc921e76884dd1cf86a91089f
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-10 20:09:37 -0300

    Update prototipo.py

--- sistema-conexao-3D-criativa/prototipo.py
+++ sistema-conexao-3D-criativa/prototipo.py
@@ -1,3 +1,10 @@
 print(f'{40 * '='}')
 print(f'{'SISTEMA CONEXÃO 3D CRIATIVA'}'.center(40))
 print(f'{40 * '='}')
+while True:
+    peso = float(input('Quantas gramas tem a peça: [Gramas]'))
+    tempo = float(input('Quanto tempo de impressão: [Horas]'))
+    res = str(input('Deseja continuar [S / N]: ')).capitalize().strip()
+    if res == 'N':
+        break
+print('FIM DO PROGRAMA')
