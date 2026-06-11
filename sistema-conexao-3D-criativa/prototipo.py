b173f25e079cc088acc0382f53962a16e05c03ee
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-11 10:16:39 -0300

    Update prototipo.py

--- sistema-conexao-3D-criativa/prototipo.py
+++ sistema-conexao-3D-criativa/prototipo.py
@@ -4,7 +4,12 @@ print(f'{40 * '='}')
 while True:
     peso = float(input('Quantas gramas tem a peça: [Gramas]'))
     tempo = float(input('Quanto tempo de impressão: [Horas]'))
+    tot = int(input('Quantas peças produzidas: '))
     res = str(input('Deseja continuar [S / N]: ')).capitalize().strip()
     if res == 'N':
         break
+#Até 5 PEÇAS - VALOR NORMAL
+#5 PEÇAS ATE 10 - 3% DESCONTO
+#A PARTIR DE 10 PEÇAS - 8% DE DESCONTO
+
 print('FIM DO PROGRAMA')
