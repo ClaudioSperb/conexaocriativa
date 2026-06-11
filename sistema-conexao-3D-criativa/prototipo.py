0d90730ec60f8623a6e88a2319b5ccc90ce897a0
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-11 12:02:12 -0300

    Update prototipo.py

--- sistema-conexao-3D-criativa/prototipo.py
+++ sistema-conexao-3D-criativa/prototipo.py
@@ -12,16 +12,17 @@ while True:
     if tot > 0 and tot <= 3:
         continue
     res = str(input('Deseja continuar [S / N] -> ')).capitalize().strip()
-# 100g x 1 hora = R$9.00 ( CUSTO MATERIAL)
-# CUSTO MÃO DE OBRA ( QUANDO HOUVER ) - R$2,03
-# CUSTO HORA MAQUINA - R$0,84
-# CUSTO DE ENERGIA - R$0,30
+    
+                        # 100g x 1 hora = R$9.00 ( CUSTO MATERIAL)
+                        # CUSTO MÃO DE OBRA ( QUANDO HOUVER ) - R$2,03
+                        # CUSTO HORA MAQUINA - R$0,84
+                        # CUSTO DE ENERGIA - R$0,30
 
     if res == 'N':
         break
     
-#Até 5 PEÇAS - VALOR NORMAL
-#5 PEÇAS ATE 10 - 3% DESCONTO
-#A PARTIR DE 10 PEÇAS - 8% DE DESCONTO
+                        #Até 5 PEÇAS - VALOR NORMAL
+                        #5 PEÇAS ATE 10 - 3% DESCONTO
+                        #A PARTIR DE 10 PEÇAS - 8% DE DESCONTO
 
 print('FIM DO PROGRAMA')
