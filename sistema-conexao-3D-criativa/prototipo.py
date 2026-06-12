264c77b0989bc3639913c6a381a13a80f00df25b
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-06-12 12:35:48 -0300

    Update prototipo.py

--- sistema-conexao-3D-criativa/prototipo.py
+++ sistema-conexao-3D-criativa/prototipo.py
@@ -28,12 +28,12 @@ while True:
     if tot <= 4 and mao_de_obra == 'S':
         print(f'O valor total Bruto é {Fore.GREEN}R${custo_mao_de_obra:.2f}{Fore.RESET}')
         print(f'{40 * '~'}')
-        print(f'Valor final de {tot} peças é {Fore.LIGHTGREEN_EX}R${tot * (custo_mao_de_obra * 4):.2f}{Fore.RESET}')
+        print(f'Valor final de {tot} peças é {Fore.LIGHTGREEN_EX}R${tot * (custo_mao_de_obra * 3):.2f}{Fore.RESET}')
         print('')
     else:
         print(f'O valor total Bruto é {Fore.GREEN}R${tot * custo_por_peça:.2f}{Fore.RESET}')
         print(f'{40 * '~'}')
-        print(f'O valor total de {tot} peças é {Fore.LIGHTGREEN_EX}R${tot * (custo_por_peça * 4):.2f}{Fore.RESET}')
+        print(f'O valor total de {tot} peças é {Fore.LIGHTGREEN_EX}R${tot * (custo_por_peça * 3):.2f}{Fore.RESET}')
         print('')
     #VALOR COM 
         
